b36514fed644aabe240c79acb5f83f7d9eec1616
lalithamanasa <manasa100998@gmail.com>
2025-04-08 11:41:18 +0100

    Initial commit

---
+++ TodoApi/Data/TodoContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using System.Collections.Generic;
+using TodoApi.Models;
+
+namespace TodoApi.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+
+        public DbSet<Todo> Todos { get; set; }
+
+        // Method to seed data if the database is empty (optional)
+        public static void Seed(TodoContext context)
+        {
+            if (!context.Todos.Any())
+            {
+                context.Todos.AddRange(
+                    new Todo { Title = "Learn C#", IsCompleted = false },
+                    new Todo { Title = "Build .NET Core App", IsCompleted = false },
+                    new Todo { Title = "Master Angular", IsCompleted = true }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ TodoApi/Models/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoApi.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
